f5610f645ac98b9dc7002faa65a883342d10e0ed
Eric Flamm <eric@flammconsulting.com>
2015-04-16 12:16:51 -0400

    Got it working (I think)
    Tested with
    source('cachematrix.R')
    > mat2<-makeCacheMatrix(mat)
    > cacheSolve(mat2)
    > cacheSolve(mat2)

--- cachematrix.R
+++ cachematrix.R
@@ -7,7 +7,7 @@ makeCacheMatrix <- function(x = matrix()) {
     inv<-NULL
     set<-function(y){
         x<<-y
-        m<<-NULL
+        inv<<-NULL
     }
     get<-function() x
     setinverse<-function(inverse) inv<<-inverse
@@ -29,7 +29,7 @@ cacheSolve <- function(x, ...) {
             return(inv)
         }
         data<-x$get()
-        inv<-inverse(data,...)
+        inv<-solve(data,...)
         x$setinverse(inv)
         inv
 }
